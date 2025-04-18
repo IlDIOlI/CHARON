f998800fe0e2ef4590e0add74f5a6ca5acfff1eb
DIO <denys_volkov@protonmail.com>
2025-04-18 20:13:53 +0200

    New function added, issues fixed...

--- testing.c
+++ testing.c
@@ -1,28 +1,19 @@
-struct Menu_init * create_struct_menu_init (char * label){
-	struct Menu_init *ptr;
-	ptr = malloc(sizeof(struct Menu_init));
-	if (ptr == NULL){
-		die(1);
-	}
-	ptr->label = strdup(label);
-	if (!ptr->label) {
-        die(2);
-    }
-	ptr->selected = false;
-	ptr->next = NULL;
-	return (ptr);
-	//REMEMBER TO FREE THIS SHI
+void item_link(struct Menu * menu_ptr,struct Menu_init * menu_in_ptr){
+		//link the head and next to each other
+		menu_ptr->head = menu_in_ptr;
 }
-struct Menu * create_struct_menu (int x,int y){
-	struct Menu *ptr;
-	ptr = malloc(sizeof(struct Menu));
-	if (ptr == NULL){
-		die(1);
-	}
-	ptr->head = NULL;
-	ptr->cursor_pos = 0;
-	ptr->x = x;
-	ptr->y = y;
-	return (ptr);
-	//REMEMBER TO FREE THIS SHI
+void menu_setup(void){
+	//setup the menu here, so I can call the function in the main just with structs defined.
+	int x,y;
+	getmaxyx(stdscr,y,x);
+	//moving so the first hardcoded text is displayed correctly
+	move(5,10);
+	x=x-10;
+	y=y-5;
+	struct Menu *main_menu = create_struct_menu(x,y);
+	//the menu is defined and is waiting for some adding options.
+	//Options are added here:
+	struct Menu_init *first_option = create_struct_menu_init("Label_the_first");
+	struct Menu_init *second_option = create_struct_menu_init("Second_label...");
+	item_link(main_menu,first_option);
 }
